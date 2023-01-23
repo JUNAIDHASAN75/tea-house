87d89e2398be9b37449937fff44c6e62d40df6fa
JUNAIDHASAN75 <spiderss907@gmail.com>
2023-01-23 13:43:43 +0600

    tea house

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  module.exports = {
+    theme: {
+      extend: {
+        zIndex: {
+          '100': '100',
+        }
+      }
+    }
+  }
